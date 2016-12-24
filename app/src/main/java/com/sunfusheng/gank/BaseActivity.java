6306b085687b6e015be9dcbde8c8aafcd3768339
sfsheng0322 <sfsheng0322@gmail.com>
2016-12-24 15:40:56 +0800

    add libs

---
+++ app/src/main/java/com/sunfusheng/gank/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.sunfusheng.gank;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.sunfusheng.gank.util.StatusBarUtil;
+import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
+
+/**
+ * Created by sunfusheng on 2016/12/24.
+ */
+
+public class BaseActivity extends RxAppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StatusBarUtil.setStatusBarTranslucent(this, true);
+    }
+}
