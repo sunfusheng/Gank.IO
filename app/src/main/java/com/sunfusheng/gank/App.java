af1efc5035ef21bbf8bb41bf6730b4c289ea4bb5
sfsheng0322 <sfsheng0322@gmail.com>
2017-01-05 17:30:48 +0800

    update libs

---
+++ app/src/main/java/com/sunfusheng/gank/App.java
@@ -0,0 +1,19 @@
+package com.sunfusheng.gank;
+
+import android.app.Application;
+
+/**
+ * Created by sunfusheng on 2017/1/5.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+//        String url = "http://click.btime.com/api/weblog";
+//        HitLog.init(this, url, true);
+//        HitLog.appStart();
+//        HitLog.deviceInfo();
+    }
+}
