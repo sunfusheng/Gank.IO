ad7065fbdf0c45322a98c009697e9e59d5f134d9
sfsheng0322 <sfsheng0322@gmail.com>
2017-01-14 00:03:30 +0800

    add gank mvp

--- app/src/main/java/com/sunfusheng/gank/base/BaseActivity.java
+++ app/src/main/java/com/sunfusheng/gank/base/BaseActivity.java
@@ -3,18 +3,34 @@ package com.sunfusheng.gank.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.orhanobut.logger.Logger;
 import com.sunfusheng.gank.util.StatusBarUtil;
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity;
 
 /**
  * Created by sunfusheng on 2016/12/24.
  */
-
-public class BaseActivity extends RxAppCompatActivity {
+public abstract class BaseActivity extends RxAppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Logger.d("log-activity", getClass().getSimpleName() + ".java");
         StatusBarUtil.setStatusBarTranslucent(this, true);
+        inflateView();
     }
+
+    private void inflateView() {
+        onBeforeInflation();
+        setContentView();
+        getWindow().setBackgroundDrawable(null);
+        onAfterInflation();
+    }
+
+    protected void onBeforeInflation() {}
+
+    protected abstract void setContentView();
+
+    protected void onAfterInflation() {}
+
 }
--- app/src/main/java/com/sunfusheng/gank/base/BaseFragment.java
+++ app/src/main/java/com/sunfusheng/gank/base/BaseFragment.java
@@ -1,20 +1,95 @@
 package com.sunfusheng.gank.base;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.orhanobut.logger.Logger;
+
 /**
  * Created by sunfusheng on 2017/1/12.
  */
 public class BaseFragment extends Fragment {
+
+    private boolean isCreated = false, isVisible = false, isEnter = false, isLoaded = false;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Logger.d("log-fragment", getClass().getSimpleName()+".java");
+        isCreated = true;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
+
+    public boolean isVisibleNow(){
+        return isCreated && isVisible;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (isVisible) {
+            onLeave();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (isVisible) {
+            onEnter();
+        }
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        isVisible = isVisibleToUser;
+        if (isCreated && isVisible) {
+            onEnter();
+            if (!isLoaded)
+                onLoad();
+        } else {
+            onLeave();
+        }
+    }
+
+    protected void onEnter() {
+        if (!isEnter && getContext() != null) {
+            isEnter = true;
+        }
+    }
+
+    private void onLeave() {
+        if (isEnter && getContext() != null) {
+            isEnter = false;
+        }
+    }
+
+    public boolean isLazyLoaded(){
+        return isLoaded;
+    }
+
+    private void onLoad() {
+        isLoaded = true;
+        new Handler(Looper.getMainLooper()).post(this::lazyLoad);
+    }
+    protected void lazyLoad(){};
+
+    public boolean onBackPressed() {return false;}
 }
