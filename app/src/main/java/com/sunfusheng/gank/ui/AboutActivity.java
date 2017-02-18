bc5722707e09518e0d277ede420a8b05c95675ee
sfsheng0322 <sfsheng0322@gmail.com>
2017-02-18 19:55:12 +0800

    change color

--- app/src/main/java/com/sunfusheng/gank/ui/AboutActivity.java
+++ app/src/main/java/com/sunfusheng/gank/ui/AboutActivity.java
@@ -2,10 +2,11 @@ package com.sunfusheng.gank.ui;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.sunfusheng.gank.R;
 import com.sunfusheng.gank.base.BaseActivity;
@@ -17,15 +18,17 @@ import butterknife.ButterKnife;
 
 /**
  * Created by sunfusheng on 15/9/8.
+ * <p>
+ * Logo 字母格式 Allura
  */
 public class AboutActivity extends BaseActivity {
 
-    @BindView(R.id.iv_back)
-    ImageView ivBack;
-    @BindView(R.id.tv_about)
-    TextView tvAbout;
     @BindView(R.id.webViewLayout)
     WebViewLayout webViewLayout;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.collapsingToolbarLayout)
+    CollapsingToolbarLayout collapsingToolbarLayout;
 
     private String url = "file:///android_asset/about_rxgank.html";
 
@@ -40,9 +43,19 @@ public class AboutActivity extends BaseActivity {
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
 
-        tvAbout.setText("关于" + AppUtil.getVersionName());
-        ivBack.setOnClickListener(v -> finish());
+        initToolBar();
         webViewLayout.loadUrl(url);
+    }
+
+    private void initToolBar() {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(v -> onBackPressed());
+        //使用CollapsingToolbarLayout必须把title设置到CollapsingToolbarLayout上，设置到Toolbar上则不会显示
+        collapsingToolbarLayout.setTitle(getString(R.string.app_name) + AppUtil.getVersionName());
+        //通过CollapsingToolbarLayout修改字体颜色
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.colorPrimary));//设置还没收缩时状态下字体颜色
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.WHITE);//设置收缩后Toolbar上字体的颜色
     }
 
     @Override
