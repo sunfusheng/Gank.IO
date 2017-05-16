41e1aa5a8d42f8050eccda8bb5fc766f0da3e472
sfsheng0322 <sfsheng0322@gmail.com>
2017-05-16 18:50:03 +0800

    add item more menu

--- app/src/main/java/com/sunfusheng/gank/viewprovider/GankItemViewProvider.java
+++ app/src/main/java/com/sunfusheng/gank/viewprovider/GankItemViewProvider.java
@@ -1,6 +1,10 @@
 package com.sunfusheng.gank.viewprovider;
 
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
@@ -9,14 +13,18 @@ import android.text.TextUtils;
 import android.text.style.ForegroundColorSpan;
 import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.sunfusheng.gank.R;
 import com.sunfusheng.gank.model.GankItem;
 import com.sunfusheng.gank.ui.WebViewActivity;
 import com.sunfusheng.gank.util.AppUtil;
+import com.sunfusheng.gank.util.ToastUtil;
 import com.sunfusheng.gank.widget.MultiType.ItemViewProvider;
 
 import butterknife.BindView;
@@ -48,16 +56,60 @@ public class GankItemViewProvider extends ItemViewProvider<GankItem, GankItemVie
             holder.tvDesc.setText(ssb);
         }
 
-        AppUtil.singleClick(holder.tvDesc, o -> {
+        AppUtil.singleClick(holder.rlGank, o -> {
             WebViewActivity.startActivity(holder.tvDesc.getContext(), item.url);
         });
 
-        holder.tvDesc.setOnLongClickListener(v -> true);
+        holder.rlGank.setOnLongClickListener(v -> true);
+
+        holder.ivMore.setOnClickListener(v -> showMoreMenu(holder.ivMore, item));
+    }
+
+    private void showMoreMenu(View anchor, GankItem gank) {
+        PopupMenu popupMenu = new PopupMenu(anchor.getContext(), anchor);
+        popupMenu.getMenuInflater().inflate(R.menu.item_more_menu, popupMenu.getMenu());
+        popupMenu.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.item_check_image:
+                    ToastUtil.show(anchor.getContext(), "开发中...");
+                    return true;
+                case R.id.item_copy_url:
+                    copy(anchor.getContext(), gank.url);
+                    return true;
+                case R.id.item_share:
+                    share(anchor.getContext(), gank.desc + "\n" + gank.url);
+                    return true;
+            }
+            return false;
+        });
+        MenuItem menuItem = popupMenu.getMenu().findItem(R.id.item_check_image);
+        menuItem.setVisible(!AppUtil.isEmpty(gank.images));
+        popupMenu.show();
+    }
+
+    public static void copy(Context context, String content) {
+        ClipboardManager cmb = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+        cmb.setText(content.trim());
+        ToastUtil.show(context, "已复制");
+    }
+
+    // 系统分享
+    public static void share(Context context, String content) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, context.getString(R.string.share));
+        intent.putExtra(Intent.EXTRA_TEXT, content);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(Intent.createChooser(intent, context.getString(R.string.share)));
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.rl_gank)
+        RelativeLayout rlGank;
         @BindView(R.id.tv_desc)
         TextView tvDesc;
+        @BindView(R.id.iv_more)
+        ImageView ivMore;
 
         ViewHolder(View view) {
             super(view);
