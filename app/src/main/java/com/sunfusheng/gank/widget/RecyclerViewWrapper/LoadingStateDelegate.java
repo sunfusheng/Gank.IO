26982adcca52829cc3aa4631af46368b8bd1dc17
sfsheng0322 <sfsheng0322@gmail.com>
2017-10-24 17:16:44 +0800

    优化代码

--- app/src/main/java/com/sunfusheng/gank/widget/RecyclerViewWrapper/LoadingStateDelegate.java
+++ app/src/main/java/com/sunfusheng/gank/widget/RecyclerViewWrapper/LoadingStateDelegate.java
@@ -12,11 +12,11 @@ import java.lang.annotation.RetentionPolicy;
  */
 public class LoadingStateDelegate {
 
-    @IntDef({STATE.LOADING, STATE.SUCCEED, STATE.ERROR, STATE.EMPTY})
+    @IntDef({STATE.SUCCEED, STATE.LOADING, STATE.ERROR, STATE.EMPTY})
     @Retention(RetentionPolicy.SOURCE)
     public @interface STATE {
-        int LOADING = 0;
-        int SUCCEED = 1;
+        int SUCCEED = 0;
+        int LOADING = 1;
         int ERROR = 2;
         int EMPTY = 3;
     }
@@ -25,14 +25,14 @@ public class LoadingStateDelegate {
     private ViewStub viewStubHolder[] = new ViewStub[4];
 
     public LoadingStateDelegate(View normalView, View loadingView, ViewStub errorStub, ViewStub emptyStub) {
-        viewHolder[0] = loadingView;
-        viewHolder[1] = normalView;
+        viewHolder[0] = normalView;
+        viewHolder[1] = loadingView;
         viewStubHolder[2] = errorStub;
         viewStubHolder[3] = emptyStub;
     }
 
-    public View setViewState(@STATE int viewState) {
-        if (viewState < 0 || viewState >= viewHolder.length) {
+    public View setViewState(@STATE int state) {
+        if (state < 0 || state >= viewHolder.length) {
             return null;
         }
 
@@ -43,20 +43,16 @@ public class LoadingStateDelegate {
             v.setVisibility(View.GONE);
         }
 
-        if (viewHolder[viewState] == null) {
-            if (viewStubHolder[viewState] != null && viewStubHolder[viewState].getParent() != null) {
-                viewHolder[viewState] = viewStubHolder[viewState].inflate();
+        if (viewHolder[state] == null) {
+            if (viewStubHolder[state] != null && viewStubHolder[state].getParent() != null) {
+                viewHolder[state] = viewStubHolder[state].inflate();
             }
         }
 
-        if (viewHolder[viewState] != null) {
-            viewHolder[viewState].setVisibility(View.VISIBLE);
+        if (viewHolder[state] != null) {
+            viewHolder[state].setVisibility(View.VISIBLE);
         }
 
-        return viewHolder[viewState];
-    }
-
-    public void setEmptyView(View view){
-        viewHolder[3] = view;
+        return viewHolder[state];
     }
 }
