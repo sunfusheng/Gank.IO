4a2a665e0a467d476e13a7c7e434e569a36a3a09
sfsheng0322 <sfsheng0322@gmail.com>
2018-01-22 14:22:50 +0800

    empty view

--- app/src/main/java/com/sunfusheng/gank/widget/EmptyView.java
+++ app/src/main/java/com/sunfusheng/gank/widget/EmptyView.java
@@ -15,10 +15,11 @@ import com.sunfusheng.gank.widget.LoadingView.LoadingView;
  * @author sunfusheng on 2018/1/20.
  */
 public class EmptyView extends FrameLayout {
-    private LoadingView mLoadingView;
-    private TextView mTitleTextView;
-    private TextView mDetailTextView;
-    protected Button mButton;
+
+    private LoadingView loadingView;
+    private TextView tvTitle;
+    private TextView tvDetail;
+    protected Button button;
 
     public EmptyView(Context context) {
         this(context, null);
@@ -30,33 +31,25 @@ public class EmptyView extends FrameLayout {
 
     public EmptyView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        init();
-        TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.EmptyView);
-        Boolean attrShowLoading = arr.getBoolean(R.styleable.EmptyView_show_loading, false);
-        String attrTitleText = arr.getString(R.styleable.EmptyView_title_text);
-        String attrDetailText = arr.getString(R.styleable.EmptyView_detail_text);
-        String attrBtnText = arr.getString(R.styleable.EmptyView_button_text);
-        arr.recycle();
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.EmptyView);
+        Boolean attrShowLoading = typedArray.getBoolean(R.styleable.EmptyView_show_loading, false);
+        String attrTitleText = typedArray.getString(R.styleable.EmptyView_title_text);
+        String attrDetailText = typedArray.getString(R.styleable.EmptyView_detail_text);
+        String attrBtnText = typedArray.getString(R.styleable.EmptyView_button_text);
+        typedArray.recycle();
         show(attrShowLoading, attrTitleText, attrDetailText, attrBtnText, null);
+
+        init();
     }
 
     private void init() {
         LayoutInflater.from(getContext()).inflate(R.layout.layout_empty_view, this, true);
-        mLoadingView = findViewById(R.id.empty_view_loading);
-        mTitleTextView = findViewById(R.id.empty_view_title);
-        mDetailTextView = findViewById(R.id.empty_view_detail);
-        mButton = findViewById(R.id.empty_view_button);
+        loadingView = findViewById(R.id.empty_view_loading);
+        tvTitle = findViewById(R.id.empty_view_title);
+        tvDetail = findViewById(R.id.empty_view_detail);
+        button = findViewById(R.id.empty_view_button);
     }
 
-    /**
-     * 显示emptyView
-     *
-     * @param loading               是否要显示loading
-     * @param titleText             标题的文字，不需要则传null
-     * @param detailText            详情文字，不需要则传null
-     * @param buttonText            按钮的文字，不需要按钮则传null
-     * @param onButtonClickListener 按钮的onClick监听，不需要则传null
-     */
     public void show(boolean loading, String titleText, String detailText, String buttonText, OnClickListener onButtonClickListener) {
         setLoadingShowing(loading);
         setTitleText(titleText);
@@ -65,11 +58,6 @@ public class EmptyView extends FrameLayout {
         show();
     }
 
-    /**
-     * 用于显示emptyView并且只显示loading的情况，此时title、detail、button都被隐藏
-     *
-     * @param loading 是否显示loading
-     */
     public void show(boolean loading) {
         setLoadingShowing(loading);
         setTitleText(null);
@@ -78,12 +66,6 @@ public class EmptyView extends FrameLayout {
         show();
     }
 
-    /**
-     * 用于显示纯文本的简单调用方法，此时loading、button均被隐藏
-     *
-     * @param titleText  标题的文字，不需要则传null
-     * @param detailText 详情文字，不需要则传null
-     */
     public void show(String titleText, String detailText) {
         setLoadingShowing(false);
         setTitleText(titleText);
@@ -92,16 +74,10 @@ public class EmptyView extends FrameLayout {
         show();
     }
 
-    /**
-     * 显示emptyView，不建议直接使用，建议调用带参数的show()方法，方便控制所有子View的显示/隐藏
-     */
     public void show() {
         setVisibility(VISIBLE);
     }
 
-    /**
-     * 隐藏emptyView
-     */
     public void hide() {
         setVisibility(GONE);
         setLoadingShowing(false);
@@ -115,34 +91,34 @@ public class EmptyView extends FrameLayout {
     }
 
     public boolean isLoading() {
-        return mLoadingView.getVisibility() == VISIBLE;
+        return loadingView.getVisibility() == VISIBLE;
     }
 
     public void setLoadingShowing(boolean show) {
-        mLoadingView.setVisibility(show ? VISIBLE : GONE);
+        loadingView.setVisibility(show ? VISIBLE : GONE);
     }
 
     public void setTitleText(String text) {
-        mTitleTextView.setText(text);
-        mTitleTextView.setVisibility(text != null ? VISIBLE : GONE);
+        tvTitle.setText(text);
+        tvTitle.setVisibility(text != null ? VISIBLE : GONE);
     }
 
     public void setDetailText(String text) {
-        mDetailTextView.setText(text);
-        mDetailTextView.setVisibility(text != null ? VISIBLE : GONE);
+        tvDetail.setText(text);
+        tvDetail.setVisibility(text != null ? VISIBLE : GONE);
     }
 
     public void setTitleColor(int color) {
-        mTitleTextView.setTextColor(color);
+        tvTitle.setTextColor(color);
     }
 
     public void setDetailColor(int color) {
-        mDetailTextView.setTextColor(color);
+        tvDetail.setTextColor(color);
     }
 
     public void setButton(String text, OnClickListener onClickListener) {
-        mButton.setText(text);
-        mButton.setVisibility(text != null ? VISIBLE : GONE);
-        mButton.setOnClickListener(onClickListener);
+        button.setText(text);
+        button.setVisibility(text != null ? VISIBLE : GONE);
+        button.setOnClickListener(onClickListener);
     }
 }
