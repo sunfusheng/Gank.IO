e1b3ec95b8ca3a97af027131d50c2ae50d8f8868
sfsheng0322 <sfsheng0322@gmail.com>
2018-06-08 11:29:10 +0800

    扩展SpannableUtil方法

--- app/src/main/java/com/sunfusheng/gank/util/SpannableUtil.java
+++ app/src/main/java/com/sunfusheng/gank/util/SpannableUtil.java
@@ -1,6 +1,8 @@
 package com.sunfusheng.gank.util;
 
+import android.content.Context;
 import android.support.annotation.ColorInt;
+import android.support.annotation.ColorRes;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.TextUtils;
@@ -10,6 +12,10 @@ import android.text.style.ForegroundColorSpan;
  * @author sunfusheng on 2018/6/8.
  */
 public class SpannableUtil {
+
+    public static SpannableString getSpannableString(Context context, String wholeText, String targetText, @ColorRes int targetTextColor) {
+        return getSpannableString(wholeText, targetText, context.getResources().getColor(targetTextColor));
+    }
 
     public static SpannableString getSpannableString(String wholeText, String targetText, @ColorInt int targetTextColor) {
         if (TextUtils.isEmpty(wholeText)) {
